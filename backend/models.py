daa9ec41d22ee94f1f89355f8cb5cf2a25a8e5b2
99x-bimsaray <bimsaray@99x.lk>
2023-07-12 22:03:38 +0530

    added the initial models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    height = models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name= 'players')
+
+class Game(models.Model):
+    played_on = models.DateTimeField()
+
+class PlayerScore(models.Model):
+    score = models.IntegerField()
+
+class TeamScore(models.Model):
+    score = models.IntegerField()
+
