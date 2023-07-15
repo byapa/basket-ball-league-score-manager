44079d0f065beca97eb0a0ff7c93055c600422ea
99x-bimsaray <bimsaray@99x.lk>
2023-07-15 15:25:09 +0530

    improved the tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,10 +1,7 @@
 from django.test import TestCase
 from django.test import TestCase, Client
-from django.urls import reverse
-from backend.views import top_players_view
 from django.core.management import call_command
 import os
-import json
 
 
 class BackendTest(TestCase):
@@ -19,10 +16,9 @@ class BackendTest(TestCase):
         call_command("loaddata", *fixtures)
 
     def test_top_players_view(self):
-
-        response = self.client.get('/api/teams/1/top-players')
+        response = self.client.get("/api/teams/1/top-players")
         self.assertEqual(response.status_code, 200)
 
-        max_numb_of_players_per_team  = 10
+        max_numb_of_players_per_team = 10
         data = response.json()
-        self.assertLess(len(data), max_numb_of_players_per_team/2)
+        self.assertLess(len(data), max_numb_of_players_per_team / 2)
